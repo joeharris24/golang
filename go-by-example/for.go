0aa104bd7776dd612e17475aa9b9bd6c38d2e76f
Joe Harris <josephbucket@gmail.com>
2021-11-06 13:07:57 +0000

    For.go addition

---
+++ go-by-example/for.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+	// Basic for loop
+	/*
+		i := 1
+		for i <= 3 {
+			fmt.Println(i)
+			i += 1
+		}
+	*/
+
+	// Boundaries for loop
+	/*
+		for j := 7; j <= 9; j++ {
+			fmt.Println(j)
+		}
+	*/
+
+	// Using break in for loop
+	/*
+		for {
+			fmt.Println("loop")
+			break
+		}
+	*/
+
+	// Using continue in for loop
+	/*
+		for n := 0; n <= 5; n++ {
+			if n%2 == 0 {
+				continue
+			}
+			fmt.Println(n)
+		}
+	*/
+
+}
