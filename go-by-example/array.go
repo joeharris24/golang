6e8771564ae2c7a9f8e1611723db6fd362a38c73
Joe Harris <josephbucket@gmail.com>
2021-11-06 13:14:09 +0000

    addition of array.go

---
+++ go-by-example/array.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	fmt.Println("emp:", a)
+	// > emp: [0 0 0 0 0]
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	// > set: [0 0 0 0 100]
+
+	fmt.Println("len: ", len(a))
+	// > len: 5
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dc1:", b)
+	// > dc1: [1 2 3 4 5]
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
+	// > 2d: [[0 1 2] [1 2 3]]
+}
